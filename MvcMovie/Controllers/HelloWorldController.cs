47f0826bcb97442d95f2eeb8a722d5af1a2e14b4
jay <jayp49943@gmail.com>
2021-07-30 18:05:05 +0530

    Initial commit

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public string Index()
+        {
+            return View();
+        // change string   return "This is my <b>default</b> action...";
+        }
+
+        public string Welcome()
+        {
+            return "This is Welcome action method";
+        }
+
+        public string Hello(string name, int numTimes = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + " NumTimes is: "+numTimes);
+        }
+
+        public string Link(string name, int ID = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + " ID: " + ID);
+            //https://localhost:44301/HelloWorld/Link/1?name=Jay
+            //https://localhost:44301/HelloWorld/Link/jay/2
+        }
+
+
+    }
+}
